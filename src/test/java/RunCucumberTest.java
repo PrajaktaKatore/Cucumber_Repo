2e8078f922c0839dc2be761edb9a8eb83c497d2a
prajaktaka <studymailid12@gmail.com>
2025-01-27 14:48:55 +0530

    m2

--- src/test/java/RunCucumberTest.java
+++ src/test/java/RunCucumberTest.java
@@ -4,7 +4,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(io.cucumber.junit.Cucumber.class)
 @CucumberOptions(
         features = "src/test/Features", // Path to the feature files
-        glue = "StepDefinitions", // Path to the step definitions package
+        glue = "src/test/java/StepDefinitions/Login.java", // Path to the step definitions package
         plugin = {"pretty", "html:target/cucumber-reports"} // Report options
 )
 public class RunCucumberTest {
